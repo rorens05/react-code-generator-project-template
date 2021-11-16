0bd306205c10deafa9b74a5e113dd5eb76698467
jeff.testproject <jeff.testproject@outlook.com>
2021-11-16 21:55:53 +0800

    Updated Spacing

--- src/views/classes/components/ClassExamHeader.js
+++ src/views/classes/components/ClassExamHeader.js
@@ -3,7 +3,7 @@ import { InputGroup, FormControl, Button } from 'react-bootstrap'
 
 function ClassExamHeader() {
   return (
-    <div>	
+  	<div>	
 			<div className="row m-b-20">
 				<div className="col-md-10 pages-header"><h1>Exam <Button Button variant="outline-warning"> <i className="fa fa-plus"></i>  Create Exam  </Button></h1>
 				</div>
@@ -14,10 +14,9 @@ function ClassExamHeader() {
 						<FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Search..." type="search"/>
 					<InputGroup.Text id="basic-addon2" className="search-button"><i className="fas fa-search fa-1x"></i></InputGroup.Text>
 					</InputGroup>
-					</div>
 				</div>
+			</div>
     </div>
   )
 }
-
 export default ClassExamHeader
