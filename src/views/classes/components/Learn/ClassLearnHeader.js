c75dc0671b56d4158391ffb222f50069ee3ba6d4
omarskie <romarisip99@gmail.com>
2021-12-07 16:58:34 +0800

    Fixed Sidenav

--- src/views/classes/components/Learn/ClassLearnHeader.js
+++ src/views/classes/components/Learn/ClassLearnHeader.js
@@ -1,6 +1,47 @@
-import React from 'react'
-import { Form, Row, Col } from 'react-bootstrap'
-function ClassLearnHeader() {
+import React, {useState, useEffect} from 'react'
+import { Form, Row, Col, NavItem } from 'react-bootstrap'
+import ClassesAPI from '../../../../api/ClassesAPI'
+import { useParams } from 'react-router'
+
+export default function ClassLearnHeader({classInfo}) {
+  console.log(classInfo?.classInformation?.courseId)
+  const [Learn, setlearn] = useState([])
+  const [moduleId, setModuleId] = useState('')
+  const {id} = useParams()
+
+  const getLearn = async() => {
+    let response = await new ClassesAPI().getLearn(classInfo?.classInformation?.courseId)
+    if(response.ok){
+      setlearn(response.data)
+    }else{
+      alert("Something went wrong while fetching all courses")
+    }
+  }
+
+  useEffect(() => {
+    getLearn()
+  }, [])
+
+  const [Pages, setPages] = useState([])
+  const [selectedUnit, setSelectedUnit] = useState([])
+  const handleSelectedUnit = (e, item) => {
+      e.preventDefault()
+      setSelectedUnit(item)
+  }
+
+  const getPages = async(e) => {
+    let response = await new ClassesAPI().getPages(id,Learn.id)
+    if(response.ok){
+      setPages(response.data)
+    }else{
+      alert("Something went wrong while fetching all courses")
+    }
+  }
+
+  useEffect(() => {
+    getPages()
+  }, [])
+
   return (
     <div>
       <div className="row m-b-20">
@@ -12,11 +53,11 @@ function ClassLearnHeader() {
         <Form.Label>Unit 1</Form.Label>
         </Col>
         <Col>
-        <Form.Select aria-label="Default select example">
-          <option>Open this select menu</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+        <Form.Select  aria-label="Default select example">
+        <option>--SELECT UNIT--</option>
+          {Learn.map(item =>{
+            return (<option value={item?.id}> {item?.moduleName}</option>)
+          })}
         </Form.Select>
         </Col>
         <Col sm={1} style={{textAlign:'center', fontSize:'20px', paddingTop:'4px', color:'#7D7D7D'}}>
@@ -24,14 +65,13 @@ function ClassLearnHeader() {
         </Col>
         <Col>
         <Form.Select aria-label="Default select example">
-          <option>Open this select menu</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          <option></option>
+          {Pages.map(item =>{
+            return (<option value={item?.courseId}> {item?.id}</option>)
+          })}
         </Form.Select>
         </Col>
       </Row>
     </div>
   )
 }
-export default ClassLearnHeader
