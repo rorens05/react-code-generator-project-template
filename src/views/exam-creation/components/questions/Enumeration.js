a0cef9b59cba1dc81f4a539393c916ab27d480a2
ferrerUnk <unkferrer@gmail.com>
2022-02-10 21:20:39 +0800

    Fix rate on editing question on enumeration

--- src/views/exam-creation/components/questions/Enumeration.js
+++ src/views/exam-creation/components/questions/Enumeration.js
@@ -156,7 +156,12 @@ export default function Enumeration({
       choices,
     };
     if (selectedQuestion != null) {
-      updateQuestion(selectedQuestion, data);
+      if(rate > 0 && rate < 101){
+        updateQuestion(selectedQuestion, data);
+      }else{
+        setLoading(false)
+        toast.error('Rate should be greater than 1 and less than 100.')
+      }
     } else {
       if(rate > 0 && rate < 101){
         addQuestion(data);
