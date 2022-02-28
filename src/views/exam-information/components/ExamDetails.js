281655e718439328b95cab354257001798763ef9
Laurence Bautista <laurence@Macbooks-MacBook-Pro.local>
2022-02-28 10:54:59 +0800

    improve exam information

--- src/views/exam-information/components/ExamDetails.js
+++ src/views/exam-information/components/ExamDetails.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
 import Status from "../../../components/utilities/Status";
+import { UserContext } from "../../../context/UserContext";
+import getStartAndEndDateFromClassTest from "../../../utils/getStartAndEndDateFromClassTest";
+import ExamStatuses from "../../classes/components/Exam/ExamStatuses";
 import ExamTimer from "./ExamTimer";
 
-const ExamParts = ({exam}) => {
+const ExamParts = ({ exam }) => {
   return exam.questionPartDto.map((part, index) => (
-    <div
-      className='exam-parts-container default-item-container'
-      key={index}
-    >
+    <div className='exam-parts-container default-item-container' key={index}>
       <p className='primary-title' style={{ fontSize: 24 }}>
         {part.questionPart.instructions}
       </p>
@@ -17,10 +17,22 @@ const ExamParts = ({exam}) => {
         style={{ fontSize: 16 }}
       >{`${part.questionDtos.length} Question(s)`}</p>
     </div>
-  ))
-}
+  ));
+};
 
-export default function ExamDetails({ exam, loading, remainingTime, startExam, examStarted, isDoneTest }) {
+export default function ExamDetails({
+  exam,
+  loading,
+  remainingTime,
+  startExam,
+  examStarted,
+  isDoneTest,
+  additionalExamInfo,
+}) {
+  const { startDate, endDate } = getStartAndEndDateFromClassTest(additionalExamInfo);
+  const userContext = useContext(UserContext);
+  const { user } = userContext.data;
+  console.log({ startDate, endDate, exam, additionalExamInfo });
   return exam != null && !loading ? (
     <div className='exam-information-container'>
       <div className='d-flex justify-content-between '>
@@ -32,24 +44,36 @@ export default function ExamDetails({ exam, loading, remainingTime, startExam, e
             {exam.test.testName}
           </p>
           <p className='secondary-title mb-2'>{`${exam.totalItems} Total Item(s)`}</p>
-          {!examStarted && (<Status>{isDoneTest ? "Completed" : "Not Started"}</Status>)}
-          {examStarted && (<Status>{"Test is Ongoing.."}</Status>)}
-          <br/>
-          {!examStarted && !isDoneTest && <Button
-            className='btn btn-primary my-4 '
-            variant='primary'
-            size='lg'
-            onClick={() => startExam()}
-          >
-            START TEST
-          </Button>}
+          {additionalExamInfo && (
+            <ExamStatuses
+              exam={additionalExamInfo}
+              user={user}
+              startDate={startDate}
+              endDate={endDate}
+            />
+          )}
+
+          {!examStarted && (
+            <Status>{isDoneTest ? "Completed" : "Not Started"}</Status>
+          )}
+          {examStarted && <Status>{"Test is Ongoing.."}</Status>}
+          <br />
+          {!examStarted && !isDoneTest && startDate < new Date() && endDate > new Date() && (
+            <Button
+              className='btn btn-primary my-4 '
+              variant='primary'
+              size='lg'
+              onClick={() => startExam()}
+            >
+              START TEST
+            </Button>
+          )}
         </div>
-        {<ExamTimer remainingTime={remainingTime} examStarted={examStarted}/>}
-        
+        {<ExamTimer remainingTime={remainingTime} examStarted={examStarted} />}
       </div>
       <hr />
       <p className='secondary-title mt-4'>Exam Parts</p>
-      { !examStarted && <ExamParts exam={exam}/>}
+      {!examStarted && <ExamParts exam={exam} />}
     </div>
   ) : (
     <div>Loading Exam Information...</div>
