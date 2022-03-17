194c1ab310065194970ed3d592070679f9af9eb4
Laurence Bautista <rorens05@gmail.com>
2022-03-17 13:00:09 +0800

    Fix report exam froala view

--- src/components/content_field/ContentViewer.js
+++ src/components/content_field/ContentViewer.js
@@ -8,12 +8,12 @@ export default function ContentViewer({className = "", children}) {
     return (
         <div className={`content-viewer ${className}`}>
           <EquationEditor
-          className='custom-input'
-          size='lg'
-          value={children.toString().split(EQUATION_TAG)[1] || ""}
-          placeholder='Enter equation'
-          autoCommands="pi theta sqrt sum prod alpha beta gamma rho"
-          autoOperatorNames="sin cos tan"
+            className='custom-input'
+            size='lg'
+            value={children.toString().split(EQUATION_TAG)[1] || ""}
+            placeholder='Enter equation'
+            autoCommands="pi theta sqrt sum prod alpha beta gamma rho"
+            autoOperatorNames="sin cos tan"
           /> 
         </div>
       )
