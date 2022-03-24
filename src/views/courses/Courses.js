a3259656c66bda309da93f954ac83593709e5e65
Krent9263 <placia.kent@gmail.com>
2022-03-24 18:10:56 +0800

    fix courses categorize

--- src/views/courses/Courses.js
+++ src/views/courses/Courses.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect, useContext } from "react";
 import CoursesItem from "../../views/courses/components/CourseItem";
 import CourseCreate from "../../views/courses/components/CourseCreate";
 import MainContainer from '../../components/layouts/MainContainer'
-import { Button, InputGroup, FormControl, CardGroup } from 'react-bootstrap';
+import { Button, InputGroup, FormControl, CardGroup, Row, Col } from 'react-bootstrap';
 import CourseEdit from "./components/CourseEdit";
 import CoursesAPI from "../../api/CoursesAPI";
 import { UserContext } from './../../context/UserContext'
+import Ict from "./subjectarea/Ict";
+import English from "./subjectarea/English";
 
 
 export default function Courses() {
@@ -18,6 +20,8 @@ export default function Courses() {
   const [openEditModal, setOpenEditModal] = useState(false)
   const [course, setCourse] = useState([])
   const [filter, setFilter] = useState("")
+  const [ictItem, setIctItem] = useState([])
+  const [englishItem, setEnglishItem] = useState([])
 	
 	const onSearch = (text) => {
     setFilter(text)
@@ -35,6 +39,11 @@ export default function Courses() {
     if(response.ok){
       const sorted = response.data.sort((a, b) => a.subjectArea.subjectAreaName > b.subjectArea.subjectAreaName ? 1:-1);
       setCourse(sorted);
+
+      const dataIct = response.data.filter((item) => item.subjectArea.subjectAreaName == 'ICT')
+      setIctItem(dataIct)
+      const dataEnglish = response.data.filter((item) => item.subjectArea.subjectAreaName == 'English')
+      setEnglishItem(dataEnglish)
     }else{
       alert("Something went wrong while fetching all courses")
     }
@@ -43,6 +52,8 @@ export default function Courses() {
   useEffect(() => {
     getCourses()
   }, [])
+
+  console.log('englishItem:', englishItem)
 
   useEffect(() => {
     if (user.isStudent) return (window.location.href = "/404");
@@ -65,8 +76,64 @@ export default function Courses() {
               </InputGroup>
             </div>
           </div>
+          <Row>
+            <Col sm={10}>
+          <div className='btn-subject-area' >
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">All</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">ICT</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">English</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">Math</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">Filipino</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">Science</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">Aralin</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">STEM</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">APPLIED</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">HUMMS</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">Robotics</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">TLE</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">HELE</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">MAPEH</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">CLE</Button>
+          </div>
+          <div style={{color:'#EE9337', fontSize:'18px',paddingTop:'4px',paddingBottom:'20px'}}> 
+             <Button className="m-r-5 color-white tficolorbg-button" size="sm">God's Park</Button>
+          </div>
+          </div>
+          </Col>
+          </Row> 
           <div className="row m-b-20 justify-content-center">
-            <CoursesItem filter={filter} setFilter={setFilter} course={course} setLoading={setLoading} setOpenEditModal={setOpenEditModal} setSelectedCourse={setSelectedCourse}/>
+            <English englishItem={englishItem} filter={filter} setFilter={setFilter} course={course} setLoading={setLoading} setOpenEditModal={setOpenEditModal} setSelectedCourse={setSelectedCourse}/>
+            {/* <Ict ictItem={ictItem} filter={filter} setFilter={setFilter} course={course} setLoading={setLoading} setOpenEditModal={setOpenEditModal} setSelectedCourse={setSelectedCourse}/> */}
+            {/* <CoursesItem filter={filter} setFilter={setFilter} course={course} setLoading={setLoading} setOpenEditModal={setOpenEditModal} setSelectedCourse={setSelectedCourse}/> */}
             <CourseCreate setCourse={setCourse} openModal={openModal} setOpenModal={setOpenModal} /> 
             <CourseEdit setCourse={setCourse} openEditModal={openEditModal} setOpenEditModal={setOpenEditModal} selectedCourse={selectedCourse} /> 
           </div>
---
+++ src/views/courses/subjectarea/English.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { Card, Dropdown, Row, Col, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import "../../../../node_modules/@fortawesome/fontawesome-free/css/all.css"
+import { Link } from 'react-router-dom'
+
+function English({englishItem, filter, setFilter, course, setLoading, setOpenEditModal, setSelectedCourse}) {
+  const [openDropdown, setOpenDropdown] = useState(false)
+  const [data, setData] = useState([])
+  
+  const handleOpeEditModal = (e, item) => {
+    e.preventDefault()
+    sessionStorage.setItem('courseid', item.id)
+    setSelectedCourse(item)
+    setOpenEditModal(true)
+  }
+
+
+  const setCourseId = (item) => {
+    sessionStorage.setItem('courseid', item)
+    sessionStorage.setItem('breadname', "Learn")
+  } 
+
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <span 
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >{children}</span>
+  ))
+
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Edit
+    </Tooltip>
+  )
+  return (
+    <>
+    {englishItem.filter(item =>
+         item.courseName.toLowerCase().includes(filter.toLowerCase())).map
+         ((item, index) => {  
+       return(
+         <Col md={3}>
+           <Link to={`coursecontent/${item.id}/learn`} onClick={() => setCourseId(item.id)} course={course} setLoading={setLoading} className="active card-title">
+           <Card className="card-design b-0px">
+             <Card.Header className="card-header-courses">
+               <Row style={{color:"white"}}>
+                   <Col md={12}>
+                     {/* <i className="fa fa-lock fa-2x"></i> */}
+                     {item.authorName !== "Techfactors Inc." &&
+                       <OverlayTrigger
+                       placement="right"
+                       delay={{ show: 10, hide: 25 }}
+                       overlay={renderTooltip}>
+                       <Dropdown className="float-right" isOpen={openDropdown} toggle={()=> setOpenDropdown(!openDropdown)}>
+                         <Dropdown.Toggle data-toggle="dropdown" as={CustomToggle} >
+                           <i className="fa fa-ellipsis-v fa-2x"></i>
+                         </Dropdown.Toggle>
+                         <Dropdown.Menu>
+                         <Dropdown.Item onClick={(e) => handleOpeEditModal(e, item)}>
+                         Edit 
+                         </Dropdown.Item>
+                         <Dropdown.Item>
+                         Delete
+                         </Dropdown.Item>
+                         </Dropdown.Menu>
+                       </Dropdown>
+                       </OverlayTrigger>
+                     }
+                   </Col>
+                   <Col md={12} className="t-a-c m-t-20">
+                     <i className="fa fa-book-open fa-7x"></i>
+                   </Col>
+               </Row>
+             </Card.Header>
+               <Card.Body>
+                   <Card.Title tag="h5">
+                       {item.courseName.substring(0, 20)}...
+                   </Card.Title>
+                   <Card.Subtitle
+                       className="mb-2 text-muted"
+                       tag="h6"
+                   >
+                       {item.subjectArea.subjectAreaName}
+                   </Card.Subtitle>
+                   <Card.Text>
+                     {item.description}
+                   </Card.Text>
+                   <Card.Text>
+                     {item.authorName}
+                   </Card.Text>
+               </Card.Body>
+           </Card>
+           </Link>
+         </Col>
+       )
+       })
+   }
+   </>
+  )
+}
+
+export default English
