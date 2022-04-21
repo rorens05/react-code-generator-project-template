c25bc4a82e5304f91d4e8fbdc071b377888edba1
Krent9263 <placia.kent@gmail.com>
2022-04-21 23:27:02 +0800

    Ongoing Feature

--- src/views/classes/components/Exam/ExamStatuses.js
+++ src/views/classes/components/Exam/ExamStatuses.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import ClassesAPI from "../../../../api/ClassesAPI";
 import Status from "../../../../components/utilities/Status";
+import ShowResultExam from "./ShowResultExam";
 
 export default function ExamStatuses({ user, exam, startDate, endDate, noAssigned }) {
-  const openModalShowResutl = (testId) => {
-    console.log('testId.', testId)
+  const[examAnalysis, setExamAnalysis] = useState([])
+
+  const openModalShowResutl = (classId, testId) => {
+    getExamAnalysis(classId, testId)
   }
+  const getExamAnalysis = async (classId, testId) =>{
+    let studentId = user?.student?.id
+    let response = await new ClassesAPI().getExamAnalysis(studentId, classId, testId)
+      if(response.ok){
+        setExamAnalysis(response.data.testPartAnswers)
+      }else{
+        alert(response.data.errorMessage)
+      }
+  }
+
+  console.log('examAnalysis:', examAnalysis)
+
   return (
     <div className='exam-status'>
       {exam?.test?.classId == null ? (<Status>Created in Course</Status>) : (<Status>Created in Class</Status>)}
@@ -20,10 +36,11 @@ export default function ExamStatuses({ user, exam, startDate, endDate, noAssigne
       {user.isStudent && (
         <>
           <Status>{exam?.isLoggedUserDone ? "Completed" : "Not Completed"}</Status>
-          <Status>{exam?.classTest?.showAnalysis ? <div onClick={() => openModalShowResutl(exam?.test?.id)}> Show Result </div> : ''}</Status>
+          <Status>{exam?.classTest?.showAnalysis ? <div  onClick={() => openModalShowResutl(exam?.classTest?.classId, exam?.test?.id)}> Show Result </div> : ''}</Status>
         </> 
       )}
       {user.isTeacher && exam?.test?.classId && exam?.test?.isShared && <Status>Shared</Status>}
+      <ShowResultExam examAnalysis={examAnalysis} />
     </div>
   );
 }
