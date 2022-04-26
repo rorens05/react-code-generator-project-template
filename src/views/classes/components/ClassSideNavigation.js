d820e718c43063564c781b9137dfd73bac4a7bcf
Michael Ben Gabriel <michaelbengabriel@Michaels-iMac.example.com>
2022-04-27 02:41:45 +0800

    scoll class, tresources

--- src/views/classes/components/ClassSideNavigation.js
+++ src/views/classes/components/ClassSideNavigation.js
@@ -117,7 +117,7 @@ export default function ClassSideNavigation({children}) {
   }
 
   return (
-    <MainContainer activeHeader={'classes'} loading={loading} fluid style='not-scrollable'>
+    <MainContainer activeHeader={'classes'} loading={loading} fluid style=''>
     <Col style={{height: 100}} />
     <Row>
       {showTab ? <Col className="row-course-bg course-widget-font" sm={3}>
@@ -150,7 +150,7 @@ export default function ClassSideNavigation({children}) {
         <Link className={currentLoc.includes('task') ? "active-nav-item" : 'nav-item'} to={`/classes/${id}/task`}>
           Task
         </Link>
-        {user?.teacher === "null" &&
+        {user?.teacher !== null &&
           <Link className={currentLoc.includes('resources') ? "active-nav-item" : 'nav-item'} to={`/classes/${id}/resources`}>
             Teacher Resources
           </Link>
