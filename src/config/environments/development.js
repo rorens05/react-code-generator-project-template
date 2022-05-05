f80531d5d36b0929a7112f7381821f6915ffca8f
Laurence Bautista <rorens05@gmail.com>
2022-05-05 12:55:40 +0800

    Update development url

--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -1,6 +1,6 @@
 const developmentConfig = {
   // API: 'https://dev-api-v3.tekteachlms.com',
-  API: 'http://api-v2-testinglang.tekteachlms.com',
+  API: 'hhttp://199.91.69.173',
   // API: 'https://dev-api.tekteachlms.com',
   // API: 'https://api-v2-testinglang.tekteachlms.com',
   SIGNALR_URL: "https://rtc.tekteachlms.com/tekteach"
