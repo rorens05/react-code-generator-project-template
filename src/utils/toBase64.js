3872752f0098464721cf090cc93a1ad7956aec7b
Laurence Bautista <rorens05@gmail.com>
2022-05-06 13:38:16 +0800

    Update base64

--- src/utils/toBase64.js
+++ src/utils/toBase64.js
@@ -2,17 +2,21 @@ const toBase64 = async(file) => {
   if(file == null){
     return null
   }
-  let data = await new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
-  return {
-    fileName: file.name,
-    base64String: data,
-    size: file.size,
-    progress: 0
+  try {
+    let data = await new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+    return {
+      fileName: file.name,
+      base64String: data,
+      size: file.size,
+      progress: 0
+    }
+  } catch (error) {
+    return null
   }
 }
 
