21afa2c6e9bca0d374aaba3f2576dcc3bcd56323
Laurence Bautista <rorens05@gmail.com>
2022-05-06 13:38:41 +0800

    Update app for toast

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import UserContextProvider from "./context/UserContext";
 import Routes from "./routes/Routes";
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
